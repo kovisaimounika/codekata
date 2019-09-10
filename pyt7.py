38aaff8666b12361124a591bd0ee2af2efee7ac8
kovisaimounika <48224745+kovisaimounika@users.noreply.github.com>
2019-09-10 14:02:14 +0530

    Add files via upload

--- pyt7.py
+++ pyt7.py
@@ -1,5 +1,12 @@
-a=[1,2,3,5,4,6,7,8]
-for i in range(8):
-    if(a[i]%2==0):
-       print(a[i])    
-    
+import math
+n=int(input())
+i=0
+b=0
+d=0
+for b in range(n):
+    for i in range(i<=n):
+        a=(i-b)
+        c=(n+a)
+        d=d+c
+print(d)       
+        
